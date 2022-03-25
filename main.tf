930d4336250f0935fd7bb8e82f72ee240e53df87
Dev <dev@round-x.com>
2022-03-26 08:03:58 +0900

    update sandbox environment

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rndx-doongle-terraform-trace"
+    bucket = "rndx-doongle-terraform-trace-sandbox"
     key    = "terraform/backend"
     region = "ap-northeast-2"
   }
@@ -9,15 +9,27 @@ terraform {
 locals {
   env_name         = "sandbox"
   aws_region       = "ap-northeast-2"
-  k8s_cluster_name = "ms-cluster"
+  k8s_cluster_name = "rndx-dev-cluster"
 }
 
+variable "mysql_password" {
+  type        = string
+  description = "Expected to be retrieved from environment variable TF_VAR_mysql_password"
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+data "aws_eks_cluster" "msur" {
+  name = module.aws-kubernetes-cluster.eks_cluster_id
+}
 # Network Configuration
 module "aws-network" {
-  source = "github.com/devsrcort/module-aws-network"
+  source = "github.com/devrndx/module-aws-network"
 
   env_name              = local.env_name
-  vpc_name              = "rndx-VPC"
+  vpc_name              = "rndx-sandbox-VPC"
   cluster_name          = local.k8s_cluster_name
   aws_region            = local.aws_region
   main_vpc_cidr         = "10.10.0.0/16"
@@ -29,9 +41,9 @@ module "aws-network" {
 
 # EKS Configuration 
 module "aws-kubernetes-cluster" {
-  source = "github.com/devsrcort/module-aws-kubernetes"
+  source = "github.com/devrndx/module-aws-kubernetes"
 
-  ms_namespace       = "rndxClientService"
+  ms_namespace       = "rndxClientSandboxService"
   env_name           = local.env_name
   aws_region         = local.aws_region
   cluster_name       = local.k8s_cluster_name
@@ -47,14 +59,62 @@ module "aws-kubernetes-cluster" {
   nodegroup_max_size       = 3
 }
 
+provider "kubernetes" {
+  #  load_config_file       = false
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.msur.endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
+  }
+}
+
+resource "kubernetes_namespace" "ms-namespace" {
+  metadata {
+    name = "microservices"
+  }
+}
+
+
 # GitOps Configuration
 module "argo-cd-server" {
   source = "github.com/devrndx/module-argo-cd"
 
-  kubernetes_cluster_id        = module.aws-kubernetes-cluster.eks_cluster_id
+  aws_region            = local.aws_region
+  kubernetes_cluster_id = data.aws_eks_cluster.msur.id
   kubernetes_cluster_name      = module.aws-kubernetes-cluster.eks_cluster_name
   kubernetes_cluster_cert_data = module.aws-kubernetes-cluster.eks_cluster_certificate_data
   kubernetes_cluster_endpoint  = module.aws-kubernetes-cluster.eks_cluster_endpoint
   eks_nodegroup_id             = module.aws-kubernetes-cluster.eks_cluster_nodegroup_id
+}
 
+module "aws-databases" {
+  source = "github.com/devrndx/ms-aws-db"
+
+  # redis_cluster_id = "redis-sandbox"
+  mysql_identifier = "mysql-sandbox"
+
+  aws_region     = local.aws_region
+  mysql_password = var.mysql_password
+  vpc_id         = module.aws-network.vpc_id
+  eks_id         = data.aws_eks_cluster.msur.id
+  eks_sg_id      = module.aws-kubernetes-cluster.eks_cluster_security_group_id
+  subnet_a_id    = module.aws-network.private_subnet_ids[0]
+  subnet_b_id    = module.aws-network.private_subnet_ids[1]
+  env_name       = local.env_name
+  route53_id     = module.aws-network.route53_id
+}
+
+# Create Traefik
+module "traefik" {
+  source = "github.com/devrndx/ms-traefik"
+
+  aws_region                   = local.aws_region
+  kubernetes_cluster_id        = data.aws_eks_cluster.msur.id
+  kubernetes_cluster_name      = module.aws-kubernetes-cluster.eks_cluster_name
+  kubernetes_cluster_cert_data = module.aws-kubernetes-cluster.eks_cluster_certificate_data
+  kubernetes_cluster_endpoint  = module.aws-kubernetes-cluster.eks_cluster_endpoint
+
+  eks_nodegroup_id = module.aws-kubernetes-cluster.eks_cluster_nodegroup_id
 }
